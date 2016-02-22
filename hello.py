2a9be8f132902f75057e7c3c1580fdc92893a055
franklingu <franklingujunchao@gmail.com>
2016-02-22 21:57:06 +0800

    added simple explanation

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    """Response with hello world only."""
+    """Response with hello world text only."""
     return 'Hello World!'
 
 if __name__ == '__main__':
